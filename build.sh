e6002fa37c2ec34e55daa5ba9c3ba6781d2f98e0
Simon <Xu>
2021-04-24 13:14:37 -0700

    update .gitignore

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-make
-cp BUILD/mbed-os-test.bin /Volumes/DIS_L4IOT
+if [ "$1" == "compile" ]; then
+    echo "compiling only"
+    make
+elif [ "$1" == "flash" ]; then
+    echo "flashing only"
+    cp BUILD/mbed-os-test.bin /Volumes/DIS_L4IOT
+else
+    echo "compiling and flashing"
+    make
+    cp BUILD/mbed-os-test.bin /Volumes/DIS_L4IOT
+fi
